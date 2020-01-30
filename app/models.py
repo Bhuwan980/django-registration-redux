b03788e6a732b0a7ede48bbfc1d86a1d1f0e9f47
bhuwan neupane <bdr.bhuwan@gmail.com>
2020-01-30 11:20:40 +0545

    this is my this third commit.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Branch(models.Model):
@@ -15,4 +16,8 @@ class Notice(models.Model):
     message = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     branch = models.ForeignKey(to=Branch,on_delete=models.CASCADE,null=True,blank=True)
-    
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.OneToOneField(to=User,on_delete=models.CASCADE)
+    branch = models.ForeignKey(to=Branch,on_delete=models.CASCADE)
